393f6968a306cf7c3c884b764d118d70553cde04
emascapa <emascapa@gmail.com>
2022-06-13 16:09:28 +0200

    Add style on movie cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,17 +5,34 @@
 @endsection
 
 @section('content')
-    <div class="container py-4">
-        <h2 class="text-center">Movies in our DataBase:</h2>
+    <div class="container py-5">
+        <h2 class="text-center mb-4">Movies in our DataBase:</h2>
 
-        @forelse ($movies as $movie)
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 g-3">
+            @forelse ($movies as $movie)
 
-        <span>{{$movie->title}}</span>
-            
-        @empty
+                <div class="col">
+                    <div class="card h-100">
+                        <img class="img-fluid" src="https://betravingknows.com/wp-content/uploads/2017/06/video-movie-placeholder-image-grey.png" alt="{{ $movie->title }}">
+                    
+                        <div class="card-body text-dark">
+                            <h4 class="text-center mb-3">{{ $movie->title  }}</h4>
+                            <ul class="list-unstyled">
 
-        <span class="lead text-center"> No movie found</span>
-            
-        @endforelse
+                                <li>Original Title: {{ $movie->original_title }}</li>
+                                <li>Nationality: {{ $movie->nationality }}</li>
+                                <li>Release date: {{ $movie->date }}</li>
+                                <li>Rating: {{ $movie->vote }}/10</li>
+                                
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+            @empty
+
+                <span class="lead text-center"> No movie found</span>
+            @endforelse
+        </div>
     </div>
 @endsection
